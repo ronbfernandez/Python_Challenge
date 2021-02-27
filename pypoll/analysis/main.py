64eff756b0f1687d7276bdfb80cb73f005067aea
Ron Fernandez <ronbfernandez@gmail.com>
2021-02-27 00:41:20 -0800

    more updates to pypoll script.

--- pypoll/analysis/main.py
+++ pypoll/analysis/main.py
@@ -5,12 +5,34 @@ election_csv = os.path.join('..', 'Resources', 'election_data.csv')
 
 csvpath=os.path.join('..', 'Resources','election_data.csv')
 
-with open(csvpath) as csvfile:
+with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    
+    candidates = {}
 
-votes = 0
-vote_count = []
-candidates = []
+    
+    for row in csvreader:
+        if row [2] in candidates.keys():
+            candidates[row[2]]+=1
+        
+        else: candidates[row[2]] = 1
+
+    total = candidates.values()
+    total_votes = sum(total)
+
+
+
+print("Election Results")
+print("-----------------")
+print(f"Total votes: {int(total_votes)}")
+print("----------------")
+
+
+
+
+
+
 
 
 
