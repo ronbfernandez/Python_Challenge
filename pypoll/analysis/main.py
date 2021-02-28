8f02cb6cbf903f8e6c92fbb477c225ba321acbd6
Ron Fernandez <ronbfernandez@gmail.com>
2021-02-27 20:14:07 -0800

    adding new updates on main.py and election_data.csv

--- pypoll/analysis/main.py
+++ pypoll/analysis/main.py
@@ -9,24 +9,51 @@ with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
     
-    candidates = {}
+    line = next(csvreader,None) 
 
-    
+
+
+    candidates = {}
+    total = candidates.values()
+    total_votes = sum(total)
+    list_candidates = candidates.keys()
+
     for row in csvreader:
-        if row [2] in candidates.keys():
+        if row[2] in candidates.keys():
             candidates[row[2]]+=1
         
         else: candidates[row[2]] = 1
 
-    total = candidates.values()
-    total_votes = sum(total)
+        total = candidates.values()
+        total_votes = sum(total) 
+
+        list_candidates = candidates.keys()
+
+    
+
+        votes_per = [f'{(x/total_votes)*100:}%' for x in candidates.values()]
+    
+
+
+
+
+    winner = list(candidates.keys())[list(candidates.values()).index(max(candidates.values()))]
+    print(winner)
 
 
 
 print("Election Results")
 print("-----------------")
 print(f"Total votes: {int(total_votes)}")
-print("----------------")
+print("-----------------")
+i = 0
+for candidate, vote in candidates.items():
+    print(f'{candidate}, {vote}, {votes_per[i]}')
+    i+=1
+print("---------------")
+print(f" Winner: {winner}")
+print("---------------")
+
 
 
 
